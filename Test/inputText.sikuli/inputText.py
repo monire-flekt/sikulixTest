fed85e86e684312938006c747f66f84d300413d1
moni <monikasrdic@re-flekt.com>
2017-08-17 15:09:31 +0200

    new folder with all stuff and sikulix.jar

---
+++ Test/inputText.sikuli/inputText.py
@@ -0,0 +1,11 @@
+name = input("Enter your name!")
+popup("Hello "+name, "Hello!")
+
+someText = inputText("Enter some text!")
+popError("Ah!" +someText)
+
+myOptions = ('1', '2', '3', '4', '5')
+result = select("Pick a number!", options = myOptions)
+choice = popAsk("Did you select"+result+ "?")
+a = "done"
+print(a)
