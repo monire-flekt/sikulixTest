af4c3a420be47efb631d2f0604155ca7c583219a
moni <monikasrdic@re-flekt.com>
2017-08-24 11:17:21 +0200

    hopefully it works now -.-

---
+++ inputText.sikuli/inputText.py
@@ -0,0 +1,12 @@
+name = input("Enter your name!")
+popup("Hello "+name, "Hello!")
+
+someText = inputText("Enter some text!")
+popError("Ah!" +someText)
+print Screen.getBounds(1)
+
+myOptions = ('1', '2', '3', '4', '5')
+result = select("Pick a number!", options = myOptions)
+choice = popAsk("Did you select"+result+ "?")
+a = "done"
+print(a)
